99c8d399944425881ffd5298328cf837e230cadc
dmtry.iudin@gmail.com <dmtry.iudin@gmail.com>
2023-01-03 21:47:06 +0200

    build: initial build

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.scss";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(<h1 className="text-orange-700">EEEEEEEEEEEEEEEEEEEEEEEEEEEEE</h1>);
